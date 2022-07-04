3771f0042c1c3b2585607699e112a68d318c79af
NakagamiYuta <guguru0014@gmail.com>
2022-07-05 01:35:34 +0900

    [add](view/auth) ログインページを動作するようにした

--- app/view/auth.py
+++ app/view/auth.py
@@ -1,4 +1,6 @@
-from flask import render_template, Blueprint
+from flask import session, request, redirect, render_template, Blueprint
+
+from db.auth import login
 
 
 route_auth = Blueprint("auth", __name__, url_prefix="/auth")
@@ -9,9 +11,17 @@ def register():
     return "Register Page"
 
 
-@route_auth.route("/login")
-def login():
+@route_auth.route("/login", methods=["GET"])
+def login_get():
     return render_template("login.html")
+
+
+@route_auth.route("/login", methods=["POST"])
+def login_post():
+    if login(request.form["userid"], request.form["password"]):
+        session["userid"] = request.form["userid"]
+        return redirect("/")
+    return render_template("login.html", warning_message="ログイン情報に誤りがあります")
 
 
 @route_auth.route("/logout")
