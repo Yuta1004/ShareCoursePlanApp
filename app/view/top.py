ca70118b8d71a27fc49d993b6e3ed8c12af56c75
NakagamiYuta <guguru0014@gmail.com>
2022-07-05 22:59:43 +0900

    [add](view) トップページでいいね機能を有効にした

--- app/view/top.py
+++ app/view/top.py
@@ -1,17 +1,29 @@
 from flask import session, request, render_template, Blueprint
 
-from db.post import get_posts
+from db.post import get_posts, incr_like_count
 
 
 route_top = Blueprint("top", __name__, url_prefix="/")
 
 
-@route_top.route("/")
-def index():
+@route_top.route("/", methods=["GET"])
+def index_get():
     page = 1
     try:
         page = int(request.args["page"])
     except:
         pass
     posts = get_posts(page)
-    return render_template("index.html", session=session, page=page, posts=posts)
+    return render_template("index.html", session=session, page=page, posts=posts, like_count=[])
+
+
+@route_top.route("/", methods=["POST"])
+def index_post():
+    incr_like_count(request.form["like"], session["id"])
+    page = 1
+    try:
+        page = int(request.args["page"])
+    except:
+        pass
+    posts = get_posts(page)
+    return render_template("index.html", session=session, page=page, posts=posts, like_count=[])
